f8b255a49d660da43cf2e1ed8632c6c1d1989cb3
Dawid Pawliszko <dawid.pawliszko@getbase.com>
2017-01-04 00:44:12 +0100

    Use Hash#[] insted of Hash#merge!
    Merge creates new hash for each iteration.

--- lib/to_lookup_hash.rb
+++ lib/to_lookup_hash.rb
@@ -3,14 +3,16 @@ require "to_lookup_hash/version"
 module Enumerable
   def to_lookup_hash
     inject({}) do |memo, element|
-      memo.merge!(yield(element) => element)
+      memo[yield(element)] = element
+      memo
     end
   end
 
   def to_lookup_hash_with_value
     inject({}) do |memo, element|
       key, value = yield(element)
-      memo.merge!(key => value)
+      memo[key] = value
+      memo
     end
   end
 end
